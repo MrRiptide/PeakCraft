84c5076e6e3f27605e8ab2c71ffb519e321bd890
MrRiptide <mrriptidemc@gmail.com>
2021-11-13 03:12:56 -0500

    starting work on custom natural spawning

---
+++ Plugin/src/main/java/io/github/mrriptide/peakcraft/runnables/PassiveMobSpawn.java
@@ -0,0 +1,28 @@
+package io.github.mrriptide.peakcraft.runnables;
+
+import org.bukkit.scheduler.BukkitRunnable;
+
+/**
+ * Passive Mob Spawner
+ *
+ * Should be called once every 400 ticks
+ *
+ * **/
+
+public class PassiveMobSpawn extends BukkitRunnable {
+    /**
+     * When an object implementing interface {@code Runnable} is used
+     * to create a thread, starting the thread causes the object's
+     * {@code run} method to be called in that separately executing
+     * thread.
+     * <p>
+     * The general contract of the method {@code run} is that it may
+     * take any action whatsoever.
+     *
+     * @see Thread#run()
+     */
+    @Override
+    public void run() {
+
+    }
+}
---
+++ Plugin/src/main/java/io/github/mrriptide/peakcraft/util/MobSpawnHelper.java
@@ -0,0 +1,6 @@
+package io.github.mrriptide.peakcraft.util;
+
+public class MobSpawnHelper {
+
+    public static void AttemptSpawnAroundPlayer
+}
